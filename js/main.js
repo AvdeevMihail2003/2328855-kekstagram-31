9806041db80b10106bacafe7f748e835bdeb1c59
Миша <misha.play.ak@gmail.com>
2024-04-15 21:49:12 +0300

    чя

--- js/main.js
+++ js/main.js
@@ -14,21 +14,9 @@ import { showError } from './4.2.js';
 
 showError();
 
-import { displayThumbnails } from './4.3.js';
-
-displayThumbnails();
-
-import { showBigPicture } from './4.4.js';
-
-showBigPicture();
-
 import { setupCloseBigPicture } from './4.5.js';
 
 setupCloseBigPicture();
-
-import { displayComments } from './4.6.js';
-
-displayComments();
 
 import { setupLoadMoreComments } from './4.7.js';
 
@@ -37,7 +25,3 @@ setupLoadMoreComments();
 import { updateCommentsCount } from './4.8.js';
 
 updateCommentsCount();
-
-import { defaultFilter, randomFilter, discussedFilter } from './5.1';
-
-defaultFilter(), randomFilter(), discussedFilter();
