579a46ce26edb6f50c279eeed37c39b85c1ec439
Миша <misha.play.ak@gmail.com>
2024-04-15 21:57:40 +0300

    яаыы

--- js/main.js
+++ js/main.js
@@ -9,19 +9,3 @@ initializeScaleControl();
 import {effectSelector} from './effectSelector.js'
 
 effectSelector();
-
-import { showError } from './4.2.js';
-
-showError();
-
-import { setupCloseBigPicture } from './4.5.js';
-
-setupCloseBigPicture();
-
-import { setupLoadMoreComments } from './4.7.js';
-
-setupLoadMoreComments();
-
-import { updateCommentsCount } from './4.8.js';
-
-updateCommentsCount();
